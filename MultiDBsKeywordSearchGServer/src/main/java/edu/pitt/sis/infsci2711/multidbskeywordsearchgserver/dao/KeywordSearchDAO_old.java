38e79f079c393d5c0d9aa16011ef98fbe506bc2d
daz45 <dlh012@Danchen-Zhang-MacBook-Air.local>
2015-03-17 13:26:25 -0400

    add ranking-danchen

--- MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/dao/KeywordSearchDAO_old.java
+++ MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/dao/KeywordSearchDAO_old.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Comparator;
 
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
@@ -52,7 +49,7 @@ public class KeywordSearchDAO {
 
 			for(String term:terms){
 			result = engine
-					.execute("MATCH p=(a)-[:`BELONG_TO`*0..]->(b) Where a.value=~\"(?i).*"+term+".*\" and b.type='database' RETURN p");
+					.execute("MATCH p=(a)-[:`BELONG_TO`*0..]->(b) Where a.value=~\"(?i).*"+term+".*\" and b.type='database'  RETURN p");
 			// resultString = result.dumpToString();
 
 			// System.out.println(resultString);
@@ -95,92 +92,9 @@ public class KeywordSearchDAO {
 		} finally {
 			db.shutdown();
 		}
-		resultSet=sort(resultSet,str);
-		// sorting
+		
+		//sorting
 		return resultSet;
 	}
-	
-	public static List<ResultModel> sort(List<ResultModel> resultSet, String str)
-	{
-		str＝str.toLowerCase();
-		Result[] rlist=new Result[resultSet.size()];
-		String[] terms=str.split(" ");
-		for(int i=0;i<resultSet.size();i++)
-		{
-			System.out.print(resultSet.get(i).getRecord()+"\t");
-			System.out.print(resultSet.get(i).getColumn()+"\t");
-			System.out.print(resultSet.get(i).getTable()+"\t");
-			System.out.print(resultSet.get(i).getDatabase()+"\n");
-			String r=resultSet.get(i).getRecord();
-			String c=resultSet.get(i).getColumn();
-			String t=resultSet.get(i).getTable();
-			String d=resultSet.get(i).getDatabase();
-			String[] s={r,c,t,d};
-			int[] scores={0,0,0,0};
-			for(int j=0;j<4;j++)
-			{
-				for(String term: terms)
-				{
-					String line=s[j];
-					line＝line.toLowerCase();
-					if(line.indexOf(term)>=0)
-						scores[j]++;
-					String[] tmps=line.split(" ");
-					for(String tmp:tmps)
-					{
-						if(tmp.equals(term))
-						{
-							scores[j]++;
-						}
-					}
-				}
-			}
-			rlist[i]=new Result(r,c,t,d,scores[0],scores[1],scores[2],scores[3]);
-			System.out.println(rlist[i].score);
-		}
-		Arrays.sort(rlist, new ResultSort());
-		resultSet=new ArrayList<ResultModel>();
-		System.out.println("\n\nResult:");
-		for(int i=0;i<rlist.length;i++)
-		{
-			resultSet.add(rlist[i].result);
-			System.out.print(resultSet.get(i).getRecord()+"\t");
-			System.out.print(resultSet.get(i).getColumn()+"\t");
-			System.out.print(resultSet.get(i).getTable()+"\t");
-			System.out.print(resultSet.get(i).getDatabase()+"\n");
-		}
-		return resultSet;
-	}
-}
-class Result
-{
-	ResultModel result;
-	double recordScore=0;
-	double columnScore=0;
-	double tableScore=0;
-	double dbScore=0;
-	double score=0;
-	public Result(String record, String column, String table, String database,double a,double b,double c,double d)
-	{
-		result=new ResultModel(record,  column, table, database);
-		this.recordScore=a;
-		this.columnScore=b;
-		this.tableScore=c;
-		this.dbScore=d;
-		this.score=a+b+c+d;
-	}
-}
-class ResultSort implements Comparator{
-    public int compare(Object obj1, Object obj2){
-    	Result o1=(Result) obj1;
-    	Result o2=(Result) obj2;
-        if(o1.score>o2.score){
-            return -1;
-        }
-        if(o1.score<o2.score){
-            return 1;
-        }
-        return 0;
-    }
-}
 
+}
